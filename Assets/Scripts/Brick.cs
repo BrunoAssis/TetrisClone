68712fc1df0fce13ed759e39127ff08054e02dc9
Bruno Assis <brunoassis@gmail.com>
2013-05-03 01:57:17 -0300

    Basic movement added.

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public string[] brickShape;
+	
+	private bool[,] cubeMatrix;
+	private float fallSpeed;
+	private int yPosition;
+	private int xPosition;
+	private int size;
+	private float halfSizeFloat;
+	private bool dropped = false;
+	
+	void Start() {
+		// Sanity checking
+		this.size = this.brickShape.Length;
+		
+		int width = this.brickShape[0].Length;
+		
+		if (this.size < 2) {
+			Debug.LogError ("Bricks must have at least two lines");
+			return;
+		}
+		
+		if (width != size) {
+			Debug.LogError ("Brick width and height must be the same");
+			return;
+		}
+		
+		if (this.size > Manager.use.maxBrickSize) {
+			Debug.LogError ("Brick must not be larger than " + Manager.use.maxBrickSize);
+			return;
+		}
+		
+		for (int i = 1; i < size; i++) {
+			if (this.brickShape[i].Length != this.brickShape[i-1].Length) {
+				Debug.LogError ("All lines in the brick must be the same length");
+				return;
+			}
+		}
+
+		this.halfSizeFloat = this.size * 0.5f; // halfSize is an integer for the array, but we need a float for positioning the on-screen cubes (for odd sizes)
+		
+		// Convert brick string array from the inspector to a boolean 2D array for easier usage
+		this.cubeMatrix = new bool[size, size];
+
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				// This [0] converts "1" string to char.
+				if (this.brickShape[y][x] == "1"[0]) {
+					this.cubeMatrix[x, y] = true;
+					Transform brick = (Transform) Instantiate(Manager.use.cube, new Vector3(x - this.halfSizeFloat, (this.size - y) + this.halfSizeFloat - size, 0.0f), Quaternion.identity);
+					brick.parent = this.transform;
+				}
+			}
+		}
+		
+		// For bricks with even sizes, we just add 0, but odd sizes need .5 added to the position to work right
+		this.transform.position = new Vector3(Manager.use.TotalFieldWidth() / 2 + (this.size % 2 == 0 ? 0.0f : 0.5f), this.transform.position.y, this.transform.position.z);
+
+		this.xPosition = (int) (this.transform.position.x - this.halfSizeFloat);
+		this.yPosition = Manager.use.TotalFieldHeight() - 1;
+
+		this.transform.position = new Vector3(this.transform.position.x, this.yPosition - this.halfSizeFloat, this.transform.position.z);
+		this.fallSpeed = Manager.use.brickNormalSpeed;
+		
+		// Check to see if this brick would overlap existing bricks, in which case the game is over
+		if (Manager.use.CheckBrickCollision(this.cubeMatrix, this.xPosition, this.yPosition)) {
+			Manager.use.GameOver();
+			return;
+		}
+	}
+	
+	void Update () {
+		this.CheckInput();
+		//yield return this.Delay((1.0f / Manager.use.brickNormalSpeed) * 2.0f);
+		this.Fall();
+	}
+	
+	private void CheckInput () {
+		float input = Input.GetAxis("Horizontal");
+		if (input < 0.0f) {
+			this.MoveHorizontal(-1);
+		} else if (input > 0.0f) {
+			this.MoveHorizontal(1);
+		}
+
+		if (Input.GetButtonDown("Rotate")) {
+			this.RotateBrick();
+		}
+
+		if (Input.GetButtonDown("Drop")) {
+			this.fallSpeed = Manager.use.brickDropSpeed;
+			this.dropped = true;
+		}
+	}
+
+	// This is used instead of WaitForSeconds, so that the delay can be cut short if player hits the drop button
+	private IEnumerator Delay (float seconds) {
+		float t = 0.0f;
+		while (t <= seconds && !dropped) {
+			t += Time.deltaTime;
+			yield return 0;
+		}
+	}
+	
+	private void Fall () {
+		// Check to see if brick would collide if moved down one row
+		this.yPosition--;
+		if (Manager.use.CheckBrickCollision(this.cubeMatrix, this.xPosition, this.yPosition)) {
+			Manager.use.SetBrick(this.cubeMatrix, this.xPosition, this.yPosition+1);
+			Destroy(this.gameObject);
+		}
+			
+		// Make on-screen brick fall down 1 square
+		// Also serves as a delay...if you want old-fashioned square-by-square movement, replace this with yield WaitForSeconds
+		for (float i = (float) this.yPosition + 1.0f; i > this.yPosition; i -= Time.deltaTime * this.fallSpeed) {
+			this.transform.position = new Vector3(this.transform.position.x, i - this.halfSizeFloat, this.transform.position.z);
+		}
+	}
+	
+	private void MoveHorizontal (int dir) {
+		// Check to see if brick could be moved in the desired direction
+		if (!Manager.use.CheckBrickCollision(this.cubeMatrix, this.xPosition + dir, this.yPosition)) {
+			this.transform.position += new Vector3(dir, 0.0f, 0.0f);
+			this.xPosition += dir;
+			//yield return new WaitForSeconds(Manager.use.brickMoveDelay);
+		}
+	}
+	
+	private void RotateBrick () {
+		// Rotate matrix 90° to the right and store the results in a temporary matrix
+		bool[,] tempMatrix = new bool[this.size, this.size];
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				tempMatrix[y, x] = this.cubeMatrix[x, (this.size - 1) - y];
+			}
+		}
+		
+		// If the rotated brick doesn't overlap existing bricks, copy the rotated matrix back and rotate on-screen brick to match
+		if (!Manager.use.CheckBrickCollision(tempMatrix, this.xPosition, this.yPosition)) {
+			System.Array.Copy (tempMatrix, this.cubeMatrix, this.size * this.size);
+			this.transform.Rotate(Vector3.forward * -90.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,205 @@
+using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	
+	public int playfieldWidth = 10;
+	public int playfieldHeight = 13;
+	public int maxBrickSize = 5;
+	public float brickNormalSpeed = 2.0f;
+	public float brickDropSpeed = 30.0f;
+	public float brickMoveDelay = 0.1f;
+	public int rowsClearedToSpeedup = 10;
+	public float speedupAmount = 0.5f;
+
+	public GameObject[] existentBricks;
+	public Transform cube;
+	public Transform leftWall;
+	public Transform rightWall;
+
+	private int totalFieldWidth;
+	private int totalFieldHeight;
+	private bool[,] playfield;
+	private Transform[] cubeReferences;
+	private int[] cubePositions;
+	private int rowsCleared = 0;
+	
+	public static Manager use;
+
+	void Start () {
+		if (!Manager.use) {
+			Manager.use = this;	// Get a reference to this script, which is a static variable so it's used as a singleton
+		}
+		
+		// Make the field larger to fit walls.
+		this.totalFieldWidth = this.playfieldWidth + this.maxBrickSize * 2;
+		
+		// Make the field taller to fit "spawn area".
+		this.totalFieldHeight = this.playfieldHeight + this.maxBrickSize;
+		
+		this.playfield = new bool[this.totalFieldWidth, this.totalFieldHeight];
+		
+		// Make the "walls" and "floor" in the array.
+		// 		true = cube of a brick
+		// 		false = open space
+		// This way we don't need special logic to deal with the bottom or edges of the playing field,
+		// since blocks will collide with the walls/floor the same as with other blocks
+		// Also, we use 0 = bottom and fieldHeight-1 = top, so that positions in the array match positions in 3D space
+		
+		// Make walls.
+		for (int i = 0; i < this.totalFieldHeight; i++) {
+			for (int j = 0; j < this.maxBrickSize; j++) {
+				this.playfield[j, i] = true;
+				this.playfield[this.totalFieldWidth-1-j, i] = true;
+			}
+		}
+		
+		// Make floor.
+		for (int i = 0; i < this.totalFieldWidth; i++) {
+			this.playfield[i, 0] = true;
+		}
+		
+		// Position stuff in the scene so it looks right regardless of what sizes are entered for the playing field
+		// (Though the camera would have to be moved back for larger sizes)
+		this.leftWall.position = new Vector3(this.maxBrickSize - 0.5f, this.leftWall.position.y, this.leftWall.position.z);
+		this.rightWall.position = new Vector3(this.totalFieldWidth - this.maxBrickSize + 0.5f, this.rightWall.position.y, this.rightWall.position.z);
+		Camera.main.transform.position = new Vector3(this.totalFieldWidth / 2, this.totalFieldHeight / 2, -16.0f);
+		
+		this.cubeReferences = new Transform[this.totalFieldWidth * this.totalFieldHeight];
+		this.cubePositions = new int[this.totalFieldWidth * this.totalFieldHeight];
+
+		this.SpawnBrick();
+	}
+	
+	private void SpawnBrick () {
+		Instantiate(this.existentBricks[Random.Range(0, this.existentBricks.Length)]);
+	}
+	
+	public int TotalFieldHeight () {
+		return this.totalFieldHeight;
+	}
+	
+	public int TotalFieldWidth () {
+		return this.totalFieldWidth;
+	}
+	
+	// See if the block matrix would overlap existing blocks in the playing field
+	// (Check from bottom-up, since in general gameplay usage it's a bit more efficient that way)
+	public bool CheckBrickCollision (bool[,] brickMatrix, int xPos, int yPos) {
+		int size = brickMatrix.GetLength(0);
+
+		for (int y = size-1; y >= 0; y--) {
+			for (int x = 0; x < size; x++) {
+				if (brickMatrix[x, y] && this.playfield[xPos+x, yPos-y]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	// Make on-screen cubes from position in array when the block is stopped from falling any more
+	// Just using DetachChildren isn't feasible because the child cubes can be in different orientations,
+	// which can mess up their position on the Y axis, which we need to be consistent in CollapseRow
+	// Also write the block matrix into the corresponding location in the playing field
+	public void SetBrick (bool[,] blockMatrix, int xPos, int yPos) {
+		int size = blockMatrix.GetLength(0);
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {	
+				if (blockMatrix[x, y]) {
+					Instantiate(cube, new Vector3(xPos+x, yPos+y, 0f), Quaternion.identity);
+					this.playfield[xPos+x, yPos-y] = true;
+				}
+			}
+		}
+		//this.CheckRows (yPos - size, size);
+		this.SpawnBrick();
+	}
+	
+	private IEnumerable CheckRows (int yStart, int size) {
+		yield return 0;	// Wait a frame for block to be destroyed so we don't include those cubes
+		
+		if (yStart < 1) { 
+			yStart = 1;	// Make sure to start above the floor
+		}
+		
+		int lastX = 0;
+		for (int y = yStart; y < yStart+size; y++) {
+			for (int x = maxBrickSize; x < totalFieldWidth-maxBrickSize; x++) { // We don't need to check the walls
+				if (!this.playfield[x, y]) break;
+				lastX = x;
+			}
+			// If the loop above completed, then x will equal fieldWidth-maxBlockSize, which means the row was completely filled in
+			if (lastX == this.totalFieldWidth - this.maxBrickSize) {
+				yield return CollapseRows (y);
+				y--; // We want to check the same row again after the collapse, in case there was more than one row filled in
+			}
+		}
+	}
+	
+	private IEnumerable CollapseRows (int yStart) {
+		// Move rows down in array, which effectively deletes the current row (yStart)
+		for (int y = yStart; y < this.totalFieldHeight-1; y++) {
+			for (int x = this.maxBrickSize; x < this.totalFieldWidth - this.maxBrickSize; x++) {
+				this.playfield[x, y] = this.playfield[x, y+1];
+			}
+		}
+
+		// Make sure top line is cleared
+		for (int x = this.maxBrickSize; x < this.totalFieldWidth - this.maxBrickSize; x++) {
+			this.playfield[x, this.totalFieldHeight-1] = false;
+		}
+		
+		// Destroy on-screen cubes on the deleted row, and store references to cubes that are above it
+		GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
+		int cubesToMove = 0;
+
+		foreach(GameObject cube in cubes) {
+			if (cube.transform.position.y > yStart) {
+				this.cubePositions[cubesToMove] = (int) cube.transform.position.y;
+				this.cubeReferences[cubesToMove++] = cube.transform;
+			} else if (cube.transform.position.y == yStart) {
+				Destroy(cube);
+			}
+		}
+
+		// Move the appropriate cubes down one square
+		// The third parameter in Mathf.Lerp is clamped to 1.0, which makes the transform.position.y be positioned exactly when done,
+		// which is important for the game logic (see the code just above)
+		float t = 0.0f;
+		while (t <= 1.0) {
+			t += Time.deltaTime * 5.0f;
+			for (int i = 0; i < cubesToMove; i++) {
+				this.cubeReferences[i].position = new Vector3(
+					this.cubeReferences[i].position.x,
+					Mathf.Lerp((float) this.cubePositions[i], (float) (this.cubePositions[i]-1), t),
+					this.cubeReferences[i].position.z
+				);
+			}
+			yield return 0;
+		}
+		
+		// Make blocks drop faster when enough rows are cleared
+		if (++this.rowsCleared == this.rowsClearedToSpeedup) {
+			this.brickNormalSpeed += this.speedupAmount;
+			this.rowsCleared = 0;
+		}
+	}
+
+	public void GameOver () {
+		Debug.Log ("Game Over!");
+	}
+
+	// Prints the state of the field array, for debugging
+	public void PrintField () {
+		string fieldChars = "";
+
+		for (int y = this.totalFieldHeight-1; y >= 0; y--) {
+			for (int x = 0; x < this.totalFieldWidth; x++) {
+				fieldChars += this.playfield[x, y] ? "1" : "0";
+			}
+			fieldChars += "\n";
+		}
+		Debug.Log (fieldChars);
+	}
+}
